078b25a47e420f340573478f7e057dc2465e70a1
PieterCorp <pieter@email.com>
2021-05-17 16:32:06 +0200

    login basics

---
+++ src/app/core/ActiveUser.ts
@@ -0,0 +1,5 @@
+export interface ActiveUser{
+    id: number,
+    firstName: string,
+    token: string,
+};
--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ActiveUser } from '../ActiveUser';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  baseUrl = 'https://localhost:44360/api/account';
+  currentUser?: ActiveUser;
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  login(model: any): Observable<any> {
+    let url = `${this.baseUrl}/login`;
+
+    return this.httpClient.post(url, model).pipe(
+      map((response: any) => {
+        const activeUser: ActiveUser = response;
+        if (activeUser) {
+          localStorage.setItem('activeUser', JSON.stringify(activeUser));
+          this.setCurrentUser(activeUser);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(activeUser: ActiveUser) {
+    this.currentUser = activeUser;
+  }
+
+  getCurrentUser(): ActiveUser | undefined {
+    if (this.currentUser) {
+      return this.currentUser;
+    }
+
+    return undefined;
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser = undefined;
+  }
 }
