1a7860cf5e7db2f2d1ef757869abb6900e4023e0
Kobe Delobelle <delobellekobe@gmail.com>
2021-05-24 19:54:17 +0200

    wallet

--- src/app/components/settings-components/vcoins/vcoins.component.ts
+++ src/app/components/settings-components/vcoins/vcoins.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TradeItem } from 'src/app/core/interfaces/tradeItem';
+import { User } from 'src/app/core/interfaces/user';
+import { TradeItemsService } from 'src/app/core/services/trade-items.service';
 
 @Component({
   selector: 'app-vcoins',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VcoinsComponent implements OnInit {
 
-  constructor() { }
+  CacheUser!: User;
+  Vcoins?: TradeItem[];
+  
+  constructor(
+    private tradeItemsService: TradeItemsService
+  ) { }
 
   ngOnInit(): void {
+    this.getUser()
+    this.getVcoinsFromLoggedInUser();
   }
 
+  getUser(): void {
+    this.CacheUser = JSON.parse(localStorage.getItem('loggedInUser') || '{}');
+  }
+
+  getVcoinsFromLoggedInUser(): void {
+    const id = this.CacheUser.id
+    this.tradeItemsService
+      .getTradeItemsFromSelectedUser(id)
+      .subscribe((x) => (this.Vcoins = x.filter(x => x.resourceId == 51 )));
+  }
 }
