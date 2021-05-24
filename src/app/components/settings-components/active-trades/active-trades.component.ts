2356177d6d7416ffeee2d24dbdcbcbdfea4a359c
Dries Maes <dm.inbox@outlook.com>
2021-05-24 16:01:55 +0200

    basic trade overview

--- src/app/components/settings-components/active-trades/active-trades.component.ts
+++ src/app/components/settings-components/active-trades/active-trades.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Trade } from 'src/app/core/interfaces/trade';
+import { User } from 'src/app/core/interfaces/user';
+import { TradeService } from 'src/app/core/services/trade.service';
 
 @Component({
   selector: 'app-active-trades',
   templateUrl: './active-trades.component.html',
-  styleUrls: ['./active-trades.component.scss']
+  styleUrls: ['./active-trades.component.scss'],
 })
 export class ActiveTradesComponent implements OnInit {
-
-  constructor() { }
+  TradeList: Trade[] = [];
+  CacheUser!: User;
+  constructor(private tradeService: TradeService) {}
 
   ngOnInit(): void {
+    this.CacheUser = JSON.parse(localStorage.getItem('loggedInUser') || '{}');
+    this.tradeService
+      .getTradesFromSelectedUser(this.CacheUser.id)
+      .subscribe((x) => {
+        this.TradeList = x;
+        console.log(this.TradeList);
+      });
   }
-
+  click() {
+    console.log(this.TradeList);
+  }
 }
