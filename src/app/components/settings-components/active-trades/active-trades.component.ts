766c26ef8303b8f86c3ee69dec3923e0bc613c66
Dries Maes <dm.inbox@outlook.com>
2021-05-24 21:01:42 +0200

    update

--- src/app/components/settings-components/active-trades/active-trades.component.ts
+++ src/app/components/settings-components/active-trades/active-trades.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Trade } from 'src/app/core/interfaces/trade';
 import { User } from 'src/app/core/interfaces/user';
 import { TradeService } from 'src/app/core/services/trade.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-active-trades',
@@ -10,19 +11,52 @@ import { TradeService } from 'src/app/core/services/trade.service';
 })
 export class ActiveTradesComponent implements OnInit {
   TradeList: Trade[] = [];
+  FullList: Trade[] = [];
   CacheUser!: User;
-  constructor(private tradeService: TradeService) {}
+  listItems: string[];
+  selectedItem: string = 'Trades to accept';
+  constructor(private tradeService: TradeService, private router: Router) {
+    this.listItems = [
+      'Proposed Trades',
+      'Trades to Accept',
+      'Completed Trades',
+      'All Trades',
+    ];
+  }
 
   ngOnInit(): void {
     this.CacheUser = JSON.parse(localStorage.getItem('loggedInUser') || '{}');
     this.tradeService
       .getTradesFromSelectedUser(this.CacheUser.id)
       .subscribe((x) => {
-        this.TradeList = x;
-        console.log(this.TradeList);
+        this.FullList = x;
+        this.TradeList = x.filter(
+          (x) => x.activeUserId == this.CacheUser.id && x.completed == false
+        );
       });
   }
-  click() {
-    console.log(this.TradeList);
+  editList() {
+    console.log('ok edit list');
+    if (this.selectedItem.includes('Proposed')) {
+      this.TradeList = [];
+      this.TradeList = this.FullList.filter(
+        (x) => x.activeUserId == this.CacheUser.id && x.completed == false
+      );
+    } else if (this.selectedItem.includes('Accept')) {
+      this.TradeList = [];
+      this.TradeList = this.FullList.filter(
+        (x) => x.activeUserId != this.CacheUser.id && x.completed == false
+      );
+    } else if (this.selectedItem.includes('Completed')) {
+      this.TradeList = [];
+      this.TradeList = this.FullList.filter((x) => x.completed == true);
+    } else if (this.selectedItem.includes('All')) {
+      this.TradeList = this.FullList;
+    }
+  }
+
+  handleClick() {
+    console.log('fezfzefez');
+    this.router.navigate(['/trade/']);
   }
 }
